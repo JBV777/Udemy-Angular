d5a2ce6c9eacb9df9bcadc444b700d71432f6f94
Shen <owner@laptop.local>
2020-05-28 18:40:32 -0400

    starting course

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//decorator: enhances class
+@Component({
+    //pass JS object to configure this
+    selector: 'app-server',
+    templateUrl: './server.component.html'    
+})
+export class ServerComponent{
+
+}
